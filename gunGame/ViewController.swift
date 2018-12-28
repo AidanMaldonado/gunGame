875a0373512d06c2f10713b7aabebbecc66fb46a
Aidan Maldonado <aidanm1198@gmail.com>
2018-12-27 20:56:43 -0500

    Basic Game
    Added Basic Game

--- gunGame/ViewController.swift
+++ gunGame/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Game Variables
+    var ammoCount = 7
+    var kills = 0
+    var ammoTotal = 100
+    
+    //Labels
+    @IBOutlet weak var ammoCountLabel: UILabel!
+    @IBOutlet weak var killsLabel: UILabel!
+    @IBOutlet weak var ammoTotalLabel: UILabel!
+    @IBOutlet weak var gunButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateLabels()
     }
-
+    
+    func updateLabels() {
+        ammoCountLabel.text = String(ammoCount)
+        killsLabel.text = "Kills: " + String(kills)
+        ammoTotalLabel.text = "Ammo: " + String(ammoTotal)
+    }
+    
+    @IBAction func shootGun() {
+        if ammoCount >= 1 && ammoTotal >= 1 {
+            ammoCount -= 1
+            ammoTotal -= 1
+            kills += 1
+        }
+        updateLabels()
+    }
+    
+    @IBAction func reloadGun() {
+        ammoCount = 7
+        updateLabels()
+    }
+    
+    @IBAction func getMoreAmmo() {
+        ammoTotal += 10
+    }
+    
+    @IBAction func changeGun() {
+        
+    }
 
 }
 
